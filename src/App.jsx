98285a1164fc28086a91bf66c48464c0fa044ad9
lgiacalo <lgiacalo@student.42.fr>
2020-11-06 16:09:53 +0100

    fix warning eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import * as api from './api';
@@ -18,12 +19,14 @@ class App extends Component {
   fetchProducts = () => {
     const { dispatch } = this.props;
     dispatch({ type: FETCH_PRODUCTS_REQUEST });
-    api
-      .fetchProducts()
-      .then((products) => {
-        dispatch({ type: FETCH_PRODUCTS_SUCCESS, products });
-      })
-      .catch((error) => dispatch({ type: FETCH_PRODUCTS_FAILURE, error }));
+    setTimeout(() => {
+      api
+        .fetchProducts()
+        .then((products) => {
+          dispatch({ type: FETCH_PRODUCTS_SUCCESS, products });
+        })
+        .catch((error) => dispatch({ type: FETCH_PRODUCTS_FAILURE, error }));
+    }, 1500);
   };
 
   render() {
@@ -42,6 +45,7 @@ class App extends Component {
             </button>
           </div>
         )}
+        {isLoading && <p>En cours de chargement</p>}
         <div className="container">
           <ProductList products={products} />
         </div>
@@ -49,6 +53,21 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  network: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+  }).isRequired,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = ({ network, products }) => ({
   network,
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import App from './App';
 
 const store = createStore(
   reducer,
+  // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
