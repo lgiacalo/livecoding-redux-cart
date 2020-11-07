2fc6957b83d8a3539534341340424460988443a2
lgiacalo <lgiacalo@student.42.fr>
2020-11-07 01:28:20 +0100

    create custom combineReducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,20 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import reducerApi from './reducerApi';
 import reducerProduct from './reducerProduct';
 import reducerCart from './reducerCart';
 
-export default combineReducers({
-  network: reducerApi,
-  products: reducerProduct,
-  cart: reducerCart,
-});
+// export default combineReducers({
+//   network: reducerApi,
+//   products: reducerProduct,
+//   cart: reducerCart,
+// });
+
+export const rootReducer = (state = {}, action) => {
+  return {
+    network: reducerApi(state.network, action),
+    products: reducerProduct(state.products, action),
+    cart: reducerCart(state.cart, action, state.products),
+  };
+};
+
+export default rootReducer;
